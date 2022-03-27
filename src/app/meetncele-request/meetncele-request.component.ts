b8e728632009e6874722c00d8a74ed4e44501291
thumai4122001 <93818724+thumai4122001@users.noreply.github.com>
2022-03-27 15:37:59 +0700

    Chỉnh component meetncele-request

--- src/app/meetncele-request/meetncele-request.component.ts
+++ src/app/meetncele-request/meetncele-request.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { customValidator } from '../validators/requestcheck.validator';
-// import { ValidationService } from 'app/validation.service';
+
 
 
 @Component({
@@ -10,18 +8,11 @@ import { customValidator } from '../validators/requestcheck.validator';
   styleUrls: ['./meetncele-request.component.css']
 })
 export class MeetnceleRequestComponent implements OnInit {
-events = ["Tiệc cưới","Hội nghị","Lễ kỉ niệm"]
-requestForm : any;
-  constructor(private _formBuilder: FormBuilder) { }
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.requestForm= this._formBuilder.group({
-      LastName:['',[Validators.required,customValidator(/\@|\$/g)]],
-      FirstName:['',[Validators.required,customValidator(/\@|\$/g)]],
-      // Email :['',Validators.compose([Validators.required, ValidationService.emailValidator])],
-      Phone : [],
-      Address : []
-    })
+    
 
   }
 }

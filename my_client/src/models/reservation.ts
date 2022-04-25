ad454868863e1df76c5b0e194bb8a3d0c80c95bd
nlmhanh110 <93784216+nlmhanh110@users.noreply.github.com>
2022-04-25 22:53:46 +0700

    cập nhật validation

--- my_client/src/models/reservation.ts
+++
@@ -1,9 +0,0 @@
-export class Reservation {
-    constructor(
-        public address: string,
-        public email: string,
-        public lastname: string, public firstname: string,
-        public paymentMethod: string,
-        public phone: string
-    ) { }
-}
